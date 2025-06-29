2cc70f5bf3f77e77167b4ceef1f613fedca5220d
famim2002 <famimhayatyash@gmail.com>
2025-06-30 01:18:00 +0600

    error404 update

--- src/pages/Error404.jsx
+++ src/pages/Error404.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
+import { FaArrowLeft } from 'react-icons/fa6'
+import { Link } from 'react-router'
 
 const Error404 = () => {
   return (
-    <section className='h-dvh'>
-        <div className='h-full flex justify-center items-center'>
-            <h1 className='text-[45px] text-black font-semibold '><span className='inline-block underline'>404</span> not found</h1>
-        </div>
+    <section className="h-dvh">
+      <Link to="/" className="p-10">
+        <FaArrowLeft className="text-[60px] absolute rounded-full hover:bg-gray-500   hover:text-white" />
+      </Link>
+      <div className="h-full flex justify-center items-center">
+        <h1 className="text-[45px] text-black font-semibold ">
+          <span className="inline-block underline">404</span> not found
+        </h1>
+      </div>
     </section>
-  )
+  );
 }
 
 export default Error404
